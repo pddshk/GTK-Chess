fab960533b4540d2a10aba753313103c5cf81ac6
Pavel Dydyshko <43893794+pddshk@users.noreply.github.com>
2023-06-17 17:01:32 +0200

    Аdd documentation (#31)
    * Add docs backbone
    * Add workflow for documentation
    * Add to .gitignore locally built docs
    * Add docs and read targets to Makefile

---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'GTK-Chess'
+copyright = '2023, Paul Dydyshko'
+author = 'Paul Dydyshko'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.githubpages',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autodoc',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'en'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
